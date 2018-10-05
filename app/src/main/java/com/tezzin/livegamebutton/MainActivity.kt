dd7e19bbc02abf3f0390bc7560dbedd6aabecfce
Claudio Tezzin Jeremias <claudio.jeremias@clansoft.com.br>
2018-10-05 09:53:38 -0300

    Buttons working and deploy configured.

--- app/src/main/java/com/tezzin/livegamebutton/MainActivity.kt
+++ app/src/main/java/com/tezzin/livegamebutton/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.tezzin.livegamebutton
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        main_button?.setOnClickListener {
+            Toast.makeText(this, "CLICKED", Toast.LENGTH_SHORT).show()
+        }
     }
 }
